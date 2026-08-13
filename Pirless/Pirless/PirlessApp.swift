7a8e48a4d932407174bdb6117242ecb1229343c5
nfahrisalim <nfahrisalim2@gmail.com>
2026-08-13 08:59:39 +0700

    feat : Add app structure

---
+++ Pirless/Pirless/PirlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  PirlessApp.swift
+//  Pirless
+//
+//  Created by Muh. Naufal Fahri Salim on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct PirlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
